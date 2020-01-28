3de2970abce6cc18508adb8a2eed788b5adf079b
Sara Marcela Zapata Saldarriaga - Ceiba Software <sara.zapata@ceiba.com.co>
2020-01-28 07:22:34 -0500

    Cobertura

--- src/main/java/com/ceiba/adn/odontoadn/aplicacion/comando/ComandoCita.java
+++ src/main/java/com/ceiba/adn/odontoadn/aplicacion/comando/ComandoCita.java
@@ -23,6 +23,4 @@ public class ComandoCita {
 	private String tarifa;
 	private Date fecha;
 
-	
-
 }
--- src/main/java/com/ceiba/adn/odontoadn/aplicacion/consulta/ConsultaCita.java
+++ src/main/java/com/ceiba/adn/odontoadn/aplicacion/consulta/ConsultaCita.java
@@ -20,48 +20,4 @@ public class ConsultaCita {
 	private String telefono;
 	private String tarifa;
 
-	
-
-	public void setEstado(String estado) {
-		this.estado = estado;
-	}
-
-	public String getMedico() {
-		return medico;
-	}
-
-	public void setMedico(String medico) {
-		this.medico = medico;
-	}
-
-	public String getTelefono() {
-		return telefono;
-	}
-
-	public void setTelefono(String telefono) {
-		this.telefono = telefono;
-	}
-
-	public String getTarifa() {
-		return tarifa;
-	}
-
-	public void setTarifa(String tarifa) {
-		this.tarifa = tarifa;
-	}
-
-	public ConsultaCita(Long id, Date fecha, String hora, String cedula, String nombre, String tipo, String estado,
-			String medico, String telefono, String tarifa) {
-		this.id = id;
-		this.fecha = fecha;
-		this.hora = hora;
-		this.cedula = cedula;
-		this.nombre = nombre;
-		this.tipo = tipo;
-		this.estado = estado;
-		this.medico = medico;
-		this.telefono = telefono;
-		this.tarifa = tarifa;
-	}
-
 }
